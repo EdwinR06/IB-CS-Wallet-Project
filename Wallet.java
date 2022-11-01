f88882ae5e6928eb452d528ba244e36a518e1426
Edwin Ryerson <eryerson06@gmail.com>
2022-11-01 12:09:57 -0600

    Add Validate Transaction Method to Wallet

--- Wallet.java
+++ Wallet.java
@@ -4,6 +4,7 @@ public class Wallet {
     private double balance;
     private ArrayList<Transaction> history;
     private String owner;
+    private boolean validFlag = false;
     
     public Wallet(String owner){
         this.balance = 0;
@@ -15,5 +16,11 @@ public class Wallet {
         return this.balance;
     }
 
+    public boolean validateTransaction(Transaction transaction) {
+        if(transaction.getAmount() != 0 && transaction.getAmount() + this.balance >= 0) {
+            validFlag = true;
+        }
+        return validFlag;
+    }
 
 }
