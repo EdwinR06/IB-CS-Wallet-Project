6510985f6c054c33f758fbbaadbc482a5c6f6bdb
Edwin Ryerson <eryerson06@gmail.com>
2022-11-01 11:40:03 -0600

    Add Transaction Constructor

---
+++ Transaction.java
@@ -0,0 +1,8 @@
+public class Transaction {
+    private double amount;
+    private String note;
+    public Transaction(double amount, String note) {
+        this.amount = amount;
+        this.note = note;
+    }
+}
