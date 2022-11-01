af1e9123fa7d3b587224573cff56ef16cd242cf6
Edwin Ryerson <eryerson06@gmail.com>
2022-11-01 11:50:25 -0600

    Add Attribute Getters to Transaction

--- Transaction.java
+++ Transaction.java
@@ -1,8 +1,17 @@
 public class Transaction {
     private double amount;
     private String note;
+
     public Transaction(double amount, String note) {
         this.amount = amount;
         this.note = note;
+    }
+
+    public double getAmount() {
+        return this.amount;
+    }
+
+    public String getNote(){
+        return this.note;
     }
 }
