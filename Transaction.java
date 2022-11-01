59446cd1898563d9c621bf6f6a2f4e41828bdb4e
Edwin Ryerson <eryerson06@gmail.com>
2022-11-01 12:07:23 -0600

    Add Transaction Constructor (no note added)

--- Transaction.java
+++ Transaction.java
@@ -7,6 +7,11 @@ public class Transaction {
         this.note = note;
     }
 
+    public Transaction(double amount) {
+        this.amount = amount;
+        this.note = "No note added";
+    }
+
     public double getAmount() {
         return this.amount;
     }
