7420954cee7a4f77ce7827814003d960bf88e35f
Edwin Ryerson <eryerson06@gmail.com>
2022-11-01 13:26:11 -0600

    Create Wallet and Transaction Instances

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.nio.channels.NonWritableChannelException;
+
+public class Main {
+    public static void main(String[] args){
+        Wallet myWallet = new Wallet("Edwin");
+        Transaction tx = new Transaction(1, "initial deposit");
+        Transaction tx1 = new Transaction(-2, "buy donut");
+        myWallet.addTransaction(tx);
+        myWallet.addTransaction(tx1);
+        myWallet.printHistory();
+
+
+    }
+}
